f35e9509b9cc6278018e00c777f82dd02d3668c8
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-11-30 17:45:29 +0100

    Update testing framework

--- src/tests/Test.hpp
+++ src/tests/Test.hpp
@@ -5,8 +5,8 @@
 class Test {
  public:
   template <typename T, typename T2>
-  static void EQU(const T& t, const T2& t2) {
-    (t == t2) ? (std::cout << "[PASSED]" << std::endl)
-              : (std::cout << "[FAILED]" << std::endl);
+  static void EQU(const std::string& name, const T& t, const T2& t2) {
+    (t == t2) ? (std::cout << "[PASSED]") : (std::cout << "[FAILED]");
+    std::cout << "\t" << name << std::endl;
   }
 };
