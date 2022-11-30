223eb9348055da09040eaf5e1e2ab4108c85380d
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-12-01 00:32:07 +0100

    More tests

--- src/tests/ProductDatabaseControllerTests.hpp
+++ src/tests/ProductDatabaseControllerTests.hpp
@@ -29,4 +29,28 @@ void productDatabaseControllerTest() {
       "productDatabaseControllerAmount",
       productDatabaseController.getProductControllerById(0).getProductAmount(),
       29);
+
+  productDatabaseController.getProductControllerById(0).setProductName(
+      "Banana");
+  productDatabaseController.getProductControllerById(0).setProductDescription(
+      "From Brazil");
+  productDatabaseController.getProductControllerById(0).setProductPrice(39.99);
+  productDatabaseController.getProductControllerById(0).setProductAmount(4999);
+
+  Test::EQU(
+      "productDatabaseControllerNameAfterChange",
+      productDatabaseController.getProductControllerById(0).getProductName(),
+      "Banana");
+  Test::EQU("productDatabaseControllerDescriptionAfterChange",
+            productDatabaseController.getProductControllerById(0)
+                .getProductDescription(),
+            "From Brazil");
+  Test::EQU(
+      "productDatabaseControllerPriceAfterChange",
+      productDatabaseController.getProductControllerById(0).getProductPrice(),
+      39.99);
+  Test::EQU(
+      "productDatabaseControllerAmountAfterChange",
+      productDatabaseController.getProductControllerById(0).getProductAmount(),
+      4999);
 }
