ced69bebeaf541410b14d8edc5389c49cfbcbbd1
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-11-30 16:56:38 +0100

    Create Product View

---
+++ src/ProductView.cpp
@@ -0,0 +1,10 @@
+#include "ProductView.hpp"
+
+void ProductView::printProductDetails(const Product& product) {
+  std::cout << "=====" << std::endl;
+  std::cout << "Name: " << product.getName() << std::endl;
+  std::cout << "Description: " << product.getDescription() << std::endl;
+  std::cout << "Price: " << product.getPrice() << std::endl;
+  std::cout << "Amount: " << product.getAmount() << std::endl;
+  std::cout << "=====" << std::endl;
+}
---
+++ src/ProductView.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+#include "Product.hpp"
+
+class ProductView {
+ public:
+  void printProductDetails(const Product& product);
+};
