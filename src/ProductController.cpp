82deda3732b2e529153dabaa1260e5d858f11035
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-11-30 17:07:59 +0100

    Create Product controller

---
+++ src/ProductController.cpp
@@ -0,0 +1,36 @@
+#include "ProductController.hpp"
+
+// Constructors
+
+ProductController::ProductController(Product& product, ProductView& productView)
+    : m_product(product), m_productView(productView) {}
+
+// Setters
+
+void ProductController::setProductName(const std::string& name) {
+  m_product.setName(name);
+}
+void ProductController::setProductDescription(const std::string& description) {
+  m_product.setDescription(description);
+}
+void ProductController::setProductPrice(double price) {
+  m_product.setPrice(price);
+}
+void ProductController::setProductAmount(unsigned long long amount) {
+  m_product.setAmount(amount);
+}
+
+// Getters
+
+std::string ProductController::getProductName() const {
+  return m_product.getName();
+}
+std::string ProductController::getProductDescription() const {
+  return m_product.getDescription();
+}
+double ProductController::getProductPrice() const {
+  return m_product.getPrice();
+}
+unsigned long long ProductController::getProductAmount() const {
+  return m_product.getAmount();
+}
---
+++ src/ProductController.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Product.hpp"
+#include "ProductView.hpp"
+
+class ProductController {
+ private:
+  Product m_product;
+  ProductView m_productView;
+
+ public:
+  ProductController(Product& product, ProductView& productView);
+
+  void setProductName(const std::string& name);
+  void setProductDescription(const std::string& description);
+  void setProductPrice(double price);
+  void setProductAmount(unsigned long long amount);
+
+  std::string getProductName() const;
+  std::string getProductDescription() const;
+  double getProductPrice() const;
+  unsigned long long getProductAmount() const;
+};
