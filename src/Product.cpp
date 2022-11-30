31318898f1c9520b95da9c24e986f83b2d645db5
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-11-30 16:49:02 +0100

    Create Product model

---
+++ src/Product.cpp
@@ -0,0 +1,34 @@
+#include "Product.hpp"
+
+// Constructors
+
+Product::Product() {
+  m_name = "UNNAMED";
+  m_description = "NONE";
+  m_price = 0;
+  m_amount = 0;
+}
+
+Product::Product(const std::string& name, const std::string& description,
+                 double price, unsigned long long amount) {
+  setName(name);
+  setDescription(description);
+  setPrice(price);
+  setAmount(amount);
+}
+
+// Setters
+
+void Product::setName(const std::string& name) { m_name = name; }
+void Product::setDescription(const std::string& description) {
+  m_description = description;
+}
+void Product::setPrice(double price) { m_price = price; }
+void Product::setAmount(unsigned long long amount) { m_amount = amount; }
+
+// Getters
+
+std::string Product::getName() const { return m_name; }
+std::string Product::getDescription() const { return m_description; }
+double Product::getPrice() const { return m_price; }
+unsigned long long Product::getAmount() const { return m_amount; }
---
+++ src/Product.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+class Product {
+ private:
+  std::string m_name;
+  std::string m_description;
+  double m_price;
+  unsigned long long m_amount;
+
+ public:
+  Product();
+  Product(const std::string& name, const std::string& description, double price,
+          unsigned long long amount);
+
+  void setName(const std::string& name);
+  void setDescription(const std::string& description);
+  void setPrice(double price);
+  void setAmount(unsigned long long amount);
+
+  std::string getName() const;
+  std::string getDescription() const;
+  double getPrice() const;
+  unsigned long long getAmount() const;
+};
