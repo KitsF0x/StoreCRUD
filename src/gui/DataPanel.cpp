cb80fb8d79cdf190ee06dcdae2b7c067f74e0376
KitsF0x <bartekmarkiewicz1799+githubacc@gmail.com>
2022-12-06 21:06:44 +0100

    Fix layout

--- src/gui/DataPanel.cpp
+++ src/gui/DataPanel.cpp
@@ -1,7 +1,6 @@
 #include "DataPanel.hpp"
 
-DataPanel::DataPanel(wxWindow* parent)
-    : wxPanel{parent, wxID_ANY, wxDefaultPosition, wxSize{1280, 720}} {
+DataPanel::DataPanel(wxWindow* parent) : wxPanel{parent} {
   SetBackgroundColour(wxColor(100, 100, 100));
   DataGrid* dataGrid = new DataGrid{this};
 }
